e8b62291e1ca9bf16e26c43fe7b6754ae8c99c11
khmuhib <khmuhib2013@gmail.com>
2022-11-01 17:37:13 +0600

    ...

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,14 @@
 <template>
   <h1 class="text-red-500 text-7xl">Home Page</h1>
+  <TextComponentVue></TextComponentVue>
 </template>
 
 <script>
+import TextComponentVue from '../components/TextComponent.vue';
 export default {
+  components: {
+    TextComponentVue
+  }
 
 }
 </script>
