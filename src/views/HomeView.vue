428ccbfcf3c7ecffee8dd479b81286e7c6178b18
khmuhib <khmuhib2013@gmail.com>
2022-11-08 18:55:46 +0600

    fontawsome add

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,20 @@
 <template>
-  <h1 class="text-red-500 text-7xl">Home Page</h1>
-  <TextComponentVue></TextComponentVue>
+  <!-- <h1 class="text-red-500 text-7xl">Home Page</h1>
+  <TextComponentVue></TextComponentVue> -->
+  <div class="">
+    <h1 class="text-6xl">Vue 3 + Tailwind + Fontawsome</h1>
+    <!-- <div class="bg-[#58AE9B]">
+      <div class="">
+        <img src="../assets/images/logo.png" alt="">
+      </div>
+      <div class="">
+        <div class="">
+          <font-awesome-icon icon="fa-solid fa-user-secret" />
+          <font-awesome-icon icon="fa-regular fa-user" />
+        </div>
+      </div>
+    </div> -->
+  </div>
 </template>
 
 <script>
